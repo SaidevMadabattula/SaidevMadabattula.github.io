5b64085da4f3e6a7594ec22c7d6b20edc0f31b24
Hyndavi M <saidevm.95@gmail.com>
2021-11-24 13:20:44 +0530

    birthday code commit app

--- src/app/birthday/birthday.component.ts
+++ src/app/birthday/birthday.component.ts
@@ -7,7 +7,7 @@ import { Observable, timer } from 'rxjs';
   templateUrl: './birthday.component.html',
   styleUrls: ['./birthday.component.css']
 })
-export class BirthdayComponent {
+export class BirthdayComponent implements OnInit {
 
   colors : Array<string> = [
     'violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',
@@ -39,31 +39,39 @@ export class BirthdayComponent {
   who! : string;
   colorSelected : string = 'red';
   source : Observable<number> = timer(1000, 500);
-
+  public loadExternalScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = true;
+    script.defer = true;
+    body.appendChild(script);
+  }
 
   constructor(private activatedRoute : ActivatedRoute) {
     this.source.subscribe((val: number) => {
          this.colorSelected = this.colors[val];                    
   });
+  }
+  ngOnInit(){
+    this.loadExternalScript('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+    this.loadExternalScript('assets/js/script.js');
+  this.loadExternalScript('assets/js/confetti.js');
 
   this.activatedRoute.queryParams.subscribe((params: Params) => {
 
-    // console.log(params);
     
     if(params['who']) {
         let names : string[] = String(params['who']).split(' '); 
         names.map((name, index, array) => {
           array[index] = name.replace(name.slice(0, 1), name.charAt(0).toUpperCase())
         });
-     // console.log('names: ', names.join(' '));
         this.who = names.join(' ');
     } else {
       this.who = 'To You';
     }
   });
-  // ngOnInit(): void {
-  // }
   
-
 }
 }
